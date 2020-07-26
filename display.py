9c727b5f68c489c82f317de800f058672fe94746
Prashant <074bex426.prashant@pcampus.edu.np>
2020-07-26 16:11:23 +0100

    updated display

--- display.py
+++ display.py
@@ -12,13 +12,11 @@ def demo(x, height=16, width=24, block_orientation=-90):
     serial = spi(port=0, device=0, gpio=noop())
     device = max7219(serial, width=width, height=height, block_orientation=block_orientation)
     
-    #draw.text((0,0), "Example", font = new_font)
+   
     with canvas(device) as draw:
-      #  textsize(str(x), font)
-      # draw.rectangle(device.bounding_box, outline="white")
-      # text(draw, (1,1), "Dist", fill="white", font=proportional(LCD_FONT))
-      # text(draw, (0, 5),str(x) , fill="white", font = new_font)
-       text(draw,(0,0), x, font=  new_font)
-    #   text(draw, (2, 10), "World", fill="white", font=proportional(LCD_FONT))
-while(1):
-    demo("Example")
+
+        draw.rectangle(device.bounding_box, outline="white")
+        text(draw, (1,1), "Dist", fill="white", font=proportional(LCD_FONT))
+        text(draw, (8,1),str(x) , fill="white", font = propertional(CP437_FONT))
+      # text(draw,(0,0), x, font=  new_font)
+      # text(draw, (2, 10), "World", fill="white", font=proportional(LCD_FONT))
